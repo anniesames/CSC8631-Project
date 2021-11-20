61b51874578723f19b6dfe2ea5d09c2c931c880b
Annie Sames <anniesames@newhuaweiapstudent-10-59-2-56.ncl.ac.uk>
2021-11-20 16:07:25 +0000

    Scatteplot of duration and % change in views

--- src/eda.R
+++ src/eda.R
@@ -9,14 +9,13 @@ h=cor(three_videos_100[,1], three_videos_100[,2])
 correlations= as.data.frame(c(e,f,g,h))
 
 #GRAPH 1: scatterplot for video duration and % of people who watched 100% (all runs)
-ggplot(data= bind_rows(data_list_100)) +
+graph1= ggplot(data= bind_rows(data_list_100)) +
   geom_point(data= seven_videos_100, aes(x=video_duration, y= viewed_onehundred_percent, col="Run 7"), alpha= 1) +
   geom_point(data= five_videos_100, aes(x=video_duration, y= viewed_onehundred_percent, col="Run 5"), alpha= 1) + 
   geom_point(data= four_videos_100, aes(x=video_duration, y= viewed_onehundred_percent, col="Run 4"), alpha= 1) +
   geom_point(data= three_videos_100, aes(x=video_duration, y= viewed_onehundred_percent, col="Run 3"), alpha= 1) +
   labs(x= "Video Duration (seconds)", y= "Percentage of people who viewed 100% of the video") +
   scale_colour_discrete(name = "Run Number")
-
 
 #GRAPH 2: scatterplot of video duration and views, split by view %
 ggplot(data= bind_rows(view_percentages_total_duration)) + 
@@ -29,9 +28,6 @@ ggplot(data= bind_rows(view_percentages_total_duration)) +
   geom_point(data= view_percentages_5_duration, aes(x=video_duration, y= viewed_five_percent, col="a. 5% of the video watched"), alpha= 1) + 
   labs(x= "Video Duration (seconds)", y= "View percentage", title= "Scatterplot of video duration and views, split by view %") + theme(legend.title = element_blank())
 
-
-graphs 
-
 #GRAPH 3: scatterplot of video number and views, split by view %
 ggplot(data= bind_rows(view_percentages_step_position)) + 
   geom_point(data= view_percentages_100_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_onehundred_percent, col="g. 100% of the video watched"), alpha= 1) +
@@ -43,18 +39,6 @@ ggplot(data= bind_rows(view_percentages_step_position)) +
   geom_point(data= view_percentages_5_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_five_percent, col="a. 5% of the video watched"), alpha= 1) + 
   labs(x= "Video Number", y= "View percentage", title= "Scatterplot of video number and views, split by view %") + theme(legend.title = element_blank()) +
   geom_vline(xintercept= 5.5) + geom_vline(xintercept= 9.5) + geom_text(aes(x=2.75, label= "Week 1", y= 90)) + geom_text(aes(x=7.5, label= "Week 2", y= 90))+ geom_text(aes(x=11.5, label= "Week 3", y= 90))
-
-ggplot(data= bind_rows(view_percentages_step_position)) + 
-  geom_point(data= view_percentages_100_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_onehundred_percent, col="g. 100% of the video watched"), alpha= 1) +
-  geom_point(data= view_percentages_95_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_ninetyfive_percent, col="f. 95% of the video watched"), alpha= 1) + 
-  geom_point(data= view_percentages_75_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_seventyfive_percent, col="e. 75% of the video watched"), alpha= 1) +
-  geom_point(data= view_percentages_50_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_fifty_percent, col="d. 50% of the video watched"), alpha= 1) +
-  geom_point(data= view_percentages_25_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_twentyfive_percent, col="c. 25% of the video watched"), alpha= 1) +
-  geom_point(data= view_percentages_10_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_ten_percent, col="b. 10% of the video watched"), alpha= 1) +
-  geom_point(data= view_percentages_5_step_position, aes(x=as.numeric(as.factor(step_position)), y= viewed_five_percent, col="a. 5% of the video watched"), alpha= 1) + 
-  labs(x= "Video Number", y= "View percentage", title= "Scatterplot of video number and views, split by view %") + theme(legend.title = element_blank()) +
-  geom_vline(xintercept= 5.5) + geom_vline(xintercept= 9.5) + geom_text(aes(x=2.75, label= "Week 1", y= 90)) + geom_text(aes(x=7.5, label= "Week 2", y= 90))+ geom_text(aes(x=11.5, label= "Week 3", y= 90))
-
 
 #GRAPH 4: scatterplot of differences in people watching 100% at start and end of each week against mean duration of time
 ggplot(data= duration_differences, aes(x=duration_differences[,1], y= viewed_onehundred_percent)) + geom_line() +
